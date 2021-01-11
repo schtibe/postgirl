b39bd501f317e9b6b3e7884e187e54734cc246ae
Stefan Heinemann <stefan.heinemann@codedump.ch>
2021-01-11 18:17:55 +0100

    Add possibility to remove headers

--- src/RequestHeaders.svelte
+++ src/RequestHeaders.svelte
@@ -1,18 +1,30 @@
 <script>
+  import * as R from "ramda";
   export let headers = [];
 
   function addHeader() {
     headers = [...headers, { name: "", value: "" }];
   }
+
+  function removeHeader(header) {
+    headers = R.without([header], headers);
+  }
 </script>
 
 <div class="flex flex-col">
-  <div>Headers:</div>
-  {#each headers as header}
-    <div class="flex">
-      <input bind:value={header.name} />
-      <input bind:value={header.value} />
-    </div>
-  {/each}
-  <button on:click={addHeader} class="w-12">Add</button>
+  <div>
+    <h2 class="text-xl">Headers:</h2>
+  </div>
+  <div class="mt-2">
+    {#each headers as header}
+      <div class="flex justify-between">
+        <input bind:value={header.name} class="w-full" />
+        <input bind:value={header.value} class="w-full ml-2" />
+        <button
+          on:click={removeHeader(header)}
+          class=" btn--secondary w-32 ml-2">-</button>
+      </div>
+    {/each}
+  </div>
+  <button on:click={addHeader} class="btn--secondary mt-2">Add Header</button>
 </div>
